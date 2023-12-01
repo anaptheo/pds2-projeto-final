e399b18465d2c89772da98982fccbbdd4a8d4b3e
victoriafloresdev <victoriafloresmello@gmail.com>
2023-12-01 13:04:45 -0300

    criacao do arquivo de formatacao

---
+++ include/formatacao.hpp
@@ -0,0 +1,18 @@
+#ifndef FORMATACAO_H
+#define FORMATACAO_H
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+#include <iomanip>
+
+
+std::string toLowerCase(const std::string& input);
+
+std::string toUpperCase(const std::string& input);
+
+std::string firstUpper(const std::string& input);
+
+
+#endif
